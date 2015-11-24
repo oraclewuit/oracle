c744bf41ebe9cfd931b82469e80b38938184ad47
Administrator <Administrator@wdp-36>
2015-11-24 09:34:30 +0800

    修改

--- spring01/src/com/no1/biz/GoodsBIZ.java
+++ spring01/src/com/no1/biz/GoodsBIZ.java
@@ -5,6 +5,10 @@ import com.no1.dao.GoodsDAO;
 public class GoodsBIZ {
 	private GoodsDAO goodsDAO;
 
+	public void show() {
+		System.out.println("hello");
+	}
+
 	public GoodsDAO getGoodsDAO() {
 		return goodsDAO;
 	}
@@ -12,8 +16,8 @@ public class GoodsBIZ {
 	public void setGoodsDAO(GoodsDAO goodsDAO) {
 		this.goodsDAO = goodsDAO;
 	}
-	
-	public void save(){
+
+	public void save() {
 		System.out.println("GoodsBIZ.save");
 		this.goodsDAO.save();
 	}
